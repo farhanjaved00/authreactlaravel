a60ec6c417dfd9a15d99cc0571463e69729d33cb
farhanjaved00 <94801120+farhanjaved00@users.noreply.github.com>
2021-11-25 00:54:35 +0500

    Api Updated
    Api Updated

--- laravelreactapi/routes/api.php
+++ laravelreactapi/routes/api.php
@@ -12,9 +12,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/checkingAuthenticated',function(){
         return response()->json(['message'=>'you are in','status'=>200],200);
     });
-    // Route::post('logout', [AuthController::class, 'logout']);
-});
+ });
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+ 
